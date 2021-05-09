665fe4e2cb56140378f273aadbf2683cb69a4649
Maksym Lytvynchuk <maksym@trynow.io>
2021-05-09 11:13:19 +0300

    Added receipt digitalizator

--- receipt_service/settings.py
+++ receipt_service/settings.py
@@ -128,3 +128,5 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MINDEE_TOKEN = os.getenv('MINDEE_TOKEN')
--- receipt_service/utils.py
+++ receipt_service/utils.py
@@ -1,7 +1,21 @@
+from mindee import Client
+from django.conf import settings
+
+
 def get_json_data_from_receipt(image_path) -> dict:
-    # TODO: Implement a function
-    return {
-        "name": "McDonalds",
-        "price": 10.22,
-        "currency": "eur"
+    client = Client(settings.MINDEE_TOKEN)
+    receipt = client.parse_receipt(image_path).receipt
+    data = {
+        'amount': receipt.total_incl.value,
+        'category': receipt.category.value,
+        'merchant_name': receipt.merchant_name.value,
+        'date': receipt.date.value,
+        'tax': receipt.total_tax.value,
     }
+    return data
+
+
+if __name__ == '__main__':
+    image_path = '/Users/maksimlitvincuk/Desktop/learning/receipt_service/media/images/receipt.png'
+    data = get_json_data_from_receipt(image_path)
+    print(data)
